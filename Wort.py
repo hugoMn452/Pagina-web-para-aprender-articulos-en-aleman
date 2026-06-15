e859e837f6489f91c5106851092973af08f32169
hugoMn452 <alconinihugomanuel@gmail.com>
2026-06-14 20:00:29 -0400

    Update Wort.py

--- Wort.py
+++ Wort.py
@@ -6,7 +6,7 @@ from streamlit.runtime.state import session_state
 
 Lista9 = ['die Wohnung', 'das Hochhaus', 'die WG', 'das Apartment', 'das Reihenhaus', 'der Raum', 'das Bad', 'die Dusche',
             'der Flur', 'die Küche', 'das Schlafzimmer', 'das Wohnzimmer', 'das Kinderzimmer', 'der Balkon', 'die Terrasse', 'der Garten', 'die Treppe', 'die Tür', 'die Decke', 'das Erdgeschoss', 'der Stock', 'die Lage', 'das Zentrum', 'der Blick', 'das Möbel', 'das Gerät',
-            'die Lampe', 'das Licht', 'die Pflanze', 'das Regal', 'der Schrank', 'der Schreibtisch', 'der Sessel', 'das Sofa', 'der Stuhl', 'der Tisch', 'der Teppich', 'das Bett', 'der Fernseher', 'das Herd', 'die Kaffemaschine', 'der Kühlschrank', 'die Spülmaschine', 'die Waschmaschine', 'die Anzeige', 'der Vertrag', 'die Miete', 'der Vermieter',
+            'die Lampe', 'das Licht', 'die Pflanze', 'das Regal', 'der Schrank', 'der Schreibtisch', 'der Sessel', 'das Sofa', 'der Stuhl', 'der Tisch', 'der Teppich', 'das Bett', 'der Fernseher', 'der Herd', 'die Kaffemaschine', 'der Kühlschrank', 'die Spülmaschine', 'die Waschmaschine', 'die Anzeige', 'der Vertrag', 'die Miete', 'der Vermieter',
             'der Umzug', 'die Feier', 'die Blume', 'der Baum', 'das Holz', 'das Heft', 'der Glückwunsch']
 
 Traduccion9 = ['Departamento', 'Casa de varios niveles', 'Vivienda comunitaria', 'Apartamento', 'Bloque de casas iguales',
